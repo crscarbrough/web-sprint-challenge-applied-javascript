e5ae05e74ee83ef132f87a4167d74f6f7eff1825
Claudia Scarbrough <rubyharrts@gmail.com>
2021-12-03 12:19:05 -0600

    finished tasks

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,7 +19,35 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
-}
+
+  const card = document.createElement("div");
+  const cHeadline = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const IMGDiv = document.createElement("div");
+  const cIMG = document.createElement("img");
+  const cAuthor = document.createElement("span");
+
+  card.classList.add("card");
+  cHeadline.classList.add("headline");
+  authorDiv.classList.add("author");
+  IMGDiv.classList.add("img-container");
+
+  cHeadline.textContent = article.headline;
+  cIMG.src = article.authorPhoto;
+  cAuthor.textContent = article.authorName;
+
+  card.appendChild(cHeadline);
+  card.appendChild(authorDiv);
+  authorDiv.appendChild(IMGDiv);
+  IMGDiv.appendChild(cIMG);
+  authorDiv.appendChild(cAuthor);
+
+  card.addEventListener("click", () => {
+    console.log(article.headline);
+  });
+
+  return card;
+};
 
 const cardAppender = (selector) => {
   // TASK 6
@@ -27,7 +57,34 @@ const cardAppender = (selector) => {
   // However, the articles do not come organized in a single, neat array. Inspect the response closely!
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
-}
 
-export { Card, cardAppender }
+  const el = document.querySelector(selector);
+
+  axios
+    .get(`http://localhost:5000/api/articles`)
+    .then((response) => {
+      response.data.articles.bootstrap.forEach((article, i) => {
+        let articleCards = Card(article);
+        el.appendChild(articleCards);
+      });
+      response.data.articles.javascript.forEach((article) => {
+        let articleCards = Card(article);
+        el.appendChild(articleCards);
+      });
+      response.data.articles.jquery.forEach((article, i) => {
+        let articleCards = Card(article);
+        el.appendChild(articleCards);
+      });
+      response.data.articles.node.forEach((article, i) => {
+        let articleCards = Card(article);
+        el.appendChild(articleCards);
+      });
+      response.data.articles.technology.forEach((article, i) => {
+        let articleCards = Card(article);
+        el.appendChild(articleCards);
+      });
+    })
+    .catch((error) => console.error(error));
+};
+
+export { Card, cardAppender };
